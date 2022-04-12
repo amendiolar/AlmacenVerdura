090db0e4ec2ccebd5aa420a931353779c5957c57
Abel Mendiola <abel.mendiola@comunidad.unam.mx>
2022-04-11 21:32:07 -0500

    Ejercicio 3

---
+++ src/AlmacenVerdura/Producto.java
@@ -0,0 +1,44 @@
+package AlmacenVerdura;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable
+{
+    private String nombre;
+    private Double precio;
+
+    public Producto()
+    {
+
+    }
+
+    public Producto(String nombre, Double precio)
+    {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
